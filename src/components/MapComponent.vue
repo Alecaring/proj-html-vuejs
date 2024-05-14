25d58db7baafa7f21d3297a0926433a4c00a3f5b
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-14 17:50:55 +0200

    fix: ComponentMap

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -28,18 +28,9 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>
-    <h1 class="text-center fs-3 px-5 mx-5 text-body-tertiary">
-      // Developers la mappa funziona è ancora in fase di sviluppo se volete
-      provarla dovete solo
-      <span class="text-danger">rimuovere il div che fa da hover.</span> <br />
-      // Bisogna studiare un modo per non far scrollare la mappa insieme allo
-      scroll della pagina
-    </h1>
-    <div class="containerMap my-5">
-      <div class="map" id="map">
-        <div class="HoverControlls"></div>
-      </div>
+  <div class="containerMap my-5">
+    <div class="map" id="map">
+      <div class="HoverControlls"></div>
     </div>
   </div>
 </template>
