b8949313db3072c09e859f1ae3df41df6ab44fdb
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-05-15 14:12:24 +0200

    Fix: sub-title video

--- src/components/AppNewBanner.vue
+++ src/components/AppNewBanner.vue
@@ -1,40 +1,54 @@
 <script>
 export default {
-    props: {
-        bannerObj: Object,
+  props: {
+    bannerObj: Object,
+  },
+  data() {
+    return {
+      bannerSev: true,
+    };
+  },
+  methods: {
+    getUrlPath(img) {
+      return new URL(`../assets/${img}.jpg`, import.meta.url).href;
     },
-    data() {
-        return {
-            bannerSev: true,
-
-        }
+    getUrlPathVideo(video) {
+      return new URL(`../assets/${video}.mp4`, import.meta.url).href;
     },
-    methods: {
-        getUrlPath(img) {
-            return new URL(`../assets/${img}.jpg`, import.meta.url).href;
-        },
-        getUrlPathVideo(video) {
-            return new URL(`../assets/${video}.mp4`, import.meta.url).href;
-        },
-    }
-
-}
+  },
+};
 </script>
 
-
 <template>
-    <div :class="bannerObj.contBanner">
-        <img :class="bannerObj.imgStyle" :src="getUrlPath(bannerObj.imgage)" alt="">
-        <video :class="bannerObj.videoStyle" playsinline autoplay loop muted :src="getUrlPathVideo(bannerObj.video)"></video>
-        <div :class="bannerObj.txtCont">
-            <h1 :class="bannerObj.bigtitle">{{ bannerObj.title }}</h1>
-            <h2 :class="bannerObj.titleSmall">{{ bannerObj.subtitle }}</h2>
-            <p :class="bannerObj.parag">{{ bannerObj.para }}</p>
-            <button :class="bannerObj.button">{{ bannerObj.buttonTxt }}</button>
-        </div>
+  <div :class="bannerObj.contBanner" class="video">
+    <img
+      :class="bannerObj.imgStyle"
+      :src="getUrlPath(bannerObj.imgage)"
+      alt=""
+    />
+    <video
+      :class="bannerObj.videoStyle"
+      playsinline
+      autoplay
+      loop
+      muted
+      :src="getUrlPathVideo(bannerObj.video)"
+    ></video>
+    <div :class="bannerObj.txtCont">
+      <h1 :class="bannerObj.bigtitle">{{ bannerObj.title }}</h1>
+      <h2 :class="bannerObj.titleSmall" class="text-black">
+        {{ bannerObj.subtitle }}
+      </h2>
+      <p :class="bannerObj.parag">{{ bannerObj.para }}</p>
+      <button :class="bannerObj.button">{{ bannerObj.buttonTxt }}</button>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.video {
+  h2 {
+    font-weight: 300;
+  }
+}
 </style>
